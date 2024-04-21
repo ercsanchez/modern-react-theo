710239aa1bf4e5497b39eb2000bcd7635cb40abd
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2024-04-22 04:50:10 +0800

    feat: protect content by requiring auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="gap flex flex-wrap gap-4">
+      {[...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48 p-4">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default function HomePage() {
+  return (
     <main className="">
-      <div className="gap flex flex-wrap gap-4">
-        {[...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48 p-4">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above.
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
