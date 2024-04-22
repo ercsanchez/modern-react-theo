f98a5a2dd0aadb80c46bf439ec269301d8daa45b
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2024-04-22 17:57:33 +0800

    feat: initial uploadthing setup and use its upload button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,14 +1,16 @@
 import { SignInButton, UserButton, SignedOut, SignedIn } from "@clerk/nextjs";
+import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
   return (
     <nav className="font-semi-bold flex w-full items-center justify-between border-b p-4 text-xl">
       <div>Gallery</div>
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton endpoint="imageUploader" />
           <UserButton />
         </SignedIn>
       </div>
